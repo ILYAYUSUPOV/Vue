cc2a4376461e153011c690be14a88c9ed9711648
ILYAYUSUPOV <ilyayusupovilya@gmail.com>
2020-09-06 14:53:14 +0300

    баги с полем ИМЯ

--- store/auth.js
+++ store/auth.js
@@ -7,10 +7,11 @@ export default {
       try {
         await firebase.auth().signInWithEmailAndPassword(email, password)
       } catch (e) {
+				commit('setError', e)
 				throw e
       }
 		},
-		async register({dispatch}, {email, password, name}) {
+		async register({dispatch, commit}, {email, password, name}) {
 			try {
 				await firebase.auth().createUserWithEmailAndPassword(email, password)
 				const uid = await dispatch('getUid')
@@ -19,6 +20,7 @@ export default {
 					name
 				})
       } catch (e) {
+				commit('setError', e)
 				throw e
       }
 		},
@@ -27,8 +29,9 @@ export default {
 			return user ? user.uid : null
 		},
 
-		async logout() {
+		async logout({commit}) {
 			await firebase.auth().signOut()
+			commit('clearInfo')
 		}
   }
 }
